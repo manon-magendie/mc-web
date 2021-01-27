7fae88de21ad46122fe2757ba0a2b209b3df8e16
Manon Magendie <manon.magendie@icloud.com>
2021-01-27 08:58:23 +0100

    wetter infos werden geholt

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
 // du JavaScript ici 
 
+const APIKey = 'd6be068477f45cf90403c8efa916dda4';
+
 let myHeading = document.querySelector('h1');
 myHeading.textContent = 'Bonjour, monde !';
 
@@ -10,6 +12,7 @@ if ("geolocation" in navigator){
         console.log(position);
         var latitude = position.coords.latitude;
         var longitude = position.coords.longitude;
+        appelAPI(latitude,longitude);
         
         var htmlLatitude = document.querySelector('#id-latitude');
         htmlLatitude.textContent = latitude ;
@@ -26,8 +29,15 @@ if ("geolocation" in navigator){
     }
 
     function noGeolocation() {
-        alert("Hallo lieber Mensch, diese Webseite funktionniert leider nur mit einem Zugriff auf deine Lokalisation!")
+        alert("Hallo lieber Mensch, diese Webseite funktionniert leider nur mit einem Zugriff auf deiner Lokalisation!")
     }
 
-
-
+    function appelAPI(latitude, longitude) {
+        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&lang=de&appid=${APIKey}`)
+        .then ((response) => {
+            return response.json();
+        })
+        .then ((data) => {
+            console.log(data);
+        })
+    }
